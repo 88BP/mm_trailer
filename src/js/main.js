5b3f7cb40457a749ad581299dd3661dd1e367928
Kate Shirley <kshirley@88brandpartners.com>
2016-05-13 11:49:13 -0500

    current iteration for 1st dev icr

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,22 @@
 console.log('main.js');
 
+$("#submit").click(function(){
+   $.ajax({url: "php/mailer.php", type: 'POST', success: function(result){
+       $("form").hide();
+   }});
+   return false;
+});
+
+function scrollToAnchor(){
+    var aTag = $('#arrow-link');
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#arrow-link").click(function() {
+   scrollToAnchor('#arrow-end');
+   console.log(' arrow clicked');
+});
+
+$('.play-container').click(function(){
+	alert("HEY GUYS! This will play a youtube video in a lightbox like AVMA microsite but need to upload the final video to youtube.")
+});
---
+++ src/php/mailer.php
@@ -0,0 +1,22 @@
+<?php require 'class.simple_mail.php';
+$name=$_POST['name']; 
+$company=$_POST['company'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+/* @var SimpleMail $mail */
+$mail = new SimpleMail();
+$mail->setTo('kshirley@88brandpartners.com', 'kshirley@88brandpartners.com')
+     ->setSubject('Test Message')
+     ->setFrom('no-reply@millennial-myth.com', 'no-reply@millenniall-myth.com')
+     ->addGenericHeader('X-Mailer', 'PHP/' . phpversion())
+     ->addGenericHeader('Content-Type', 'text/html; charset="utf-8"')
+     ->setMessage($name)
+     ->setWrap(78);
+$send = $mail->send();
+//echo $mail->debug();
+if ($send) {
+    echo 'Email was sent successfully!';
+} else {
+    echo 'An error occurred. We could not send email';
+}
